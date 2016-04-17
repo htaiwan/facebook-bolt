bfbc718b214d33e920b2f5d90443706a95a4351a
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2016-04-17 19:54:39 +0800

    remove redundant code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var _ = require('underscore');
 var token = '7788';
 var page_token = 'CAAN5IdgZBuDsBAHFJFwFxNO0OdXQfVrqZAwsQ2tH54ynGxu830cFTTjhzctbogucZBGrGUZCdGKVCfAlIZCizZClzZC95WkPlaL219IbSsfppouEEXP2YJEkPUVeN2293lnJhq9Lqw0z7NYJ6QSM9VGYdHTgxsybDqIkNRkxvtHSAWZADBGgob355gRKaSs9wDsZD';
 
@@ -54,21 +53,6 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
-
-// app.post('/webhook/', function (req, res) {
-// 	messaging_events = req.body.entry[0].messaging;
-// 	for (i = 0; i < messaging_events.length; i++){
-// 		event = res.body.entry[0].messaging[i];
-// 		sender = event.sender.id;
-// 		if (event.message && event.message.text) {
-// 			text = event.message.text;
-// 			// Handle a text message from this sender
-// 			sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
-// 		}
-// 	};
-// 	res.sendStatus(200);
-// });
-
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
